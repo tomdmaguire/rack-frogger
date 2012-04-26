ad00c15675ee107aafa651e0f801dcda19a9990e
Tom Maguire <tom@musicglue.com>
2012-04-26 16:13:06 +0100

    first commit

---
+++ lib/rack/frogger.rb
@@ -0,0 +1,49 @@
+require 'yaml'
+
+module Rack
+  class Frogger
+    def initialize app, config_path, logger = nil
+      @app = app
+      @logger = logger
+      config = YAML::load( ::File.open( config_path ) )
+      @urls = config['request_path_filter']
+      @status_codes = config['status_filter']
+      @use_rails_logger = config['use_rails_logger']
+      @log_method = config['log_method']
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+      status, header, response = @app.call(env)
+      log env, request, status, header, response if @urls.include?(request.path) && @status_codes.include?(status)
+      [status, header, response]
+    end
+
+    private
+
+    def log(env, request, status, header, response)
+      logger = @logger || (@use_rails_logger ? Rails.logger : env['rack.errors'])
+      request.body.rewind
+      log_method = @log_method || 'info'
+      logger.send log_method, <<-LOG
+#{Time.now.utc}
+  URL: #{request.url}
+REQUEST:
+#{formatted_request env}
+  Body: #{request.body.read}
+RESPONSE:
+#{formatted_response_header header}
+  Status: #{status}
+  Body: #{response.body}
+LOG
+    end
+
+    def formatted_request env
+      env.keys.find_all { |k| k =~ /^[A-Z_]+$/ }.map { |k| "  #{k.gsub('_', ' ').gsub('HTTP', '').lstrip.capitalize} : #{env[k]}" }.join "\n"
+    end
+
+    def formatted_response_header header
+      Utils::HeaderHash.new(header).map { |k,v| "  #{k} : #{v}" }.join "\n"
+    end
+  end
+end
